6b746d4b514913a815a1692551899b9f7428b13e
qizwiz <jonathan.f.hill@gmail.com>
2012-07-19 22:20:04 -0500

    initial rails app with bootstrap

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'mysql2'
+gem 'twitter-bootstrap-rails', :git => 'https://github.com/qizwiz/twitter-bootstrap-rails.git'
+gem 'rspec'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapExample::Application.config.secret_token = '154f3ebfc7fa4ccc6a93b097d8cd8195a6cec30d5c7ec589b9a55dd4e4695a5096f9cd7e17d846ebdca0bc2f4e8c07507d2d7165145f1ab4a09150d3985c40aa'
